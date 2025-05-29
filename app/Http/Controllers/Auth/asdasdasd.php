2971542be2c05d1d38ea95fe641e66d12c648718
wille ferreira <wille26ferreira@gmail.com>
2025-05-28 21:07:52 -0400

    corrigindo nomeclatura de arquivo

---
+++ app/Http/Controllers/Auth/asdasdasd.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Modules\Auth\Login\Authenticate;
+use App\Modules\Auth\Login\generateAuthCookies;
+use Illuminate\Http\JsonResponse;
+
+class LoginController extends Controller
+{
+    public function __construct(
+        private Authenticate $authenticate,
+        private generateAuthCookies $createToken
+    ) {
+
+    }
+    public function login(LoginRequest $request): JsonResponse
+    {
+        $user = $this->authenticate->auth($request);
+        return $this->createToken->generate($user);
+    }
+}
