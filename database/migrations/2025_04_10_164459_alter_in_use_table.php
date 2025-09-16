ac2457bacab4983de238c43407b04590eb44fabb
wille <wille@clickmulti.com.br>
2025-09-16 16:32:20 -0400

    fix: corrige nome da tabela no método down da migração

--- database/migrations/2025_04_10_164459_alter_in_use_table.php
+++ database/migrations/2025_04_10_164459_alter_in_use_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('use', function (Blueprint $table) {
+        Schema::table('users', function (Blueprint $table) {
             //
             $table->dropColumn('role');
         });
