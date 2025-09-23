0f4b90c38f567f7962503c2b6a744725c242feee
wille <wille@clickmulti.com.br>
2025-09-23 13:31:34 -0400

    fix: add missing boolean validation rules for novoaltura, novopeso, and quadril fields in AnuncianteDadosRequest

--- app/Http/Controllers/Anunciante/Requests/AnuncianteDadosRequest.php
+++ app/Http/Controllers/Anunciante/Requests/AnuncianteDadosRequest.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace App\Http\Controllers\Anunciante\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
@@ -18,9 +19,13 @@ class AnuncianteDadosRequest extends FormRequest
             'cache_acompanhante' => 'nullable|string',
             'cartao_acompanhante' => 'nullable|string',
             'novoaltura_acompanhante' => 'nullable|string',
+            "novoaltura_esconder" => 'nullable|boolean',
             'novopeso_acompanhante' => 'nullable|string',
+            "novopeso_esconder" => 'nullable|boolean',
             'quadril_acompanhante' => 'nullable|string',
+            "quadril_esconder" => 'nullable|boolean',
             'novopes_acompanhante' => 'nullable|string',
+            'novopes_esconder' => 'nullable|boolean',
             'novoacompanha_acompanhante' => 'nullable|string'
         ];
     }
