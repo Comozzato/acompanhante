8efb6bd61f228dfc0ec3b8be6f4c6ef53ae5e6f0
Vitor Calderaro <vhsantanamao@gmail.com>
2025-10-15 18:05:50 -0400

    Update Posts.php

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -34,7 +34,7 @@ class Posts extends Model
     public function feeds()
     {
         return $this->hasMany(Feed::class, 'post_id')
-            ->lastTreePosts()
+            //->lastTreePosts()
             ->aprovado()
             ->story();
     }
